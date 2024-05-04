1a47764ec4048c5c27252ecc43fd300a5e807e6c
pedroastro <pedromscastro9095@gmail.com>
2024-05-04 05:49:49 -0300

    add domains

--- site_casamento/settings.py
+++ site_casamento/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0dkcb@%a2d2(jukii+2*b!_l(adggf!!u+j*#c(-ch(ju^_w&$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'isabelaepedro.site', 'www.isabelaepedro.site']
 
 
 # Application definition
