a89c45810cced537ad19230d1ce451223c505d28
Pedro Castro <68872502+Pedroastro@users.noreply.github.com>
2024-06-09 08:11:51 -0300

    Update views.py

--- home/views.py
+++ home/views.py
@@ -53,7 +53,7 @@ def calculate_total_donated(dictionary):
     return total_donated
 
 def calculate_progress(total_donated):
-    progress = ((total_donated - 10000)*100)/AMMOUNT_NEEDED
+    progress = ((total_donated - 11500)*100)/AMMOUNT_NEEDED
     print(progress)
     return round(progress, 2)
 
